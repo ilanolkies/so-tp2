804508e1a515dac3d99d3df499dbf7e1b24ff88d
Ilan Olkies <ilanolkies@outlook.com>
2019-11-18 00:38:26 -0300

    Fix print

--- node.cpp
+++ node.cpp
@@ -13,8 +13,8 @@ int total_nodes, mpi_rank;
 Block *last_block_in_chain;
 map<string, Block> node_blocks;
 
-//Cuando me llega una cadena adelantada, y tengo que pedir los nodos que me faltan
-//Si nos separan más de VALIDATION_BLOCKS bloques de distancia entre las cadenas, se descarta por seguridad
+// Cuando me llega una cadena adelantada, y tengo que pedir los nodos que me faltan
+// Si nos separan más de VALIDATION_BLOCKS bloques de distancia entre las cadenas, se descarta por seguridad
 bool verificar_y_migrar_cadena(const Block *rBlock, const MPI_Status *status)
 {
   //TODO: Enviar mensaje TAG_CHAIN_HASH
@@ -30,8 +30,6 @@ bool verificar_y_migrar_cadena(const Block *rBlock, const MPI_Status *status)
 
   delete[] blockchain;
 
-  printf("[%d] Perdí la carrera por uno (%d) contra %d \n", mpi_rank, rBlock->index, status->MPI_SOURCE);
-
   return false;
 }
 
@@ -42,7 +40,7 @@ bool agregar_como_ultimo_bloque(const Block *rBlock, const MPI_Status *status)
   return true;
 }
 
-//Verifica que el bloque tenga que ser incluido en la cadena, y lo agrega si corresponde
+// Verifica que el bloque tenga que ser incluido en la cadena, y lo agrega si corresponde
 bool validate_block_for_chain(const Block *rBlock, const MPI_Status *status)
 {
   if (valid_new_block(rBlock))
@@ -68,6 +66,7 @@ bool validate_block_for_chain(const Block *rBlock, const MPI_Status *status)
 
       // pero el bloque anterior apuntado por el recibido no es mí último actual,
       // entonces hay una blockchain más larga que la mía.
+      printf("[%d] Perdí la carrera por uno (%d) contra %d \n", mpi_rank, rBlock->index, status->MPI_SOURCE);
       return verificar_y_migrar_cadena(rBlock, status);
     }
 
@@ -100,7 +99,7 @@ bool validate_block_for_chain(const Block *rBlock, const MPI_Status *status)
   return false;
 }
 
-//Envia el bloque minado a todos los nodos
+// Envia el bloque minado a todos los nodos
 void broadcast_block(const Block *block)
 {
   for (uint i = 0; i < total_nodes; i++)
@@ -108,7 +107,7 @@ void broadcast_block(const Block *block)
       MPI_Send(block, 1, *MPI_BLOCK, i, TAG_NEW_BLOCK, MPI_COMM_WORLD);
 }
 
-//Proof of work
+// Proof of work
 //TODO: Advertencia: puede tener condiciones de carrera
 void *proof_of_work(void *ptr)
 {
@@ -119,23 +118,23 @@ void *proof_of_work(void *ptr)
   {
     block = *last_block_in_chain;
 
-    //Preparar nuevo bloque
+    // Preparar nuevo bloque
     block.index += 1;
     block.node_owner_number = mpi_rank;
     block.difficulty = DEFAULT_DIFFICULTY;
     block.created_at = static_cast<unsigned long int>(time(NULL));
     memcpy(block.previous_block_hash, block.block_hash, HASH_SIZE);
 
-    //Agregar un nonce al azar al bloque para intentar resolver el problema
+    // Agregar un nonce al azar al bloque para intentar resolver el problema
     gen_random_nonce(block.nonce);
 
-    //Hashear el contenido (con el nuevo nonce)
+    // Hashear el contenido (con el nuevo nonce)
     block_to_hash(&block, hash_hex_str);
 
-    //Contar la cantidad de ceros iniciales (con el nuevo nonce)
+    // Contar la cantidad de ceros iniciales (con el nuevo nonce)
     if (solves_problem(hash_hex_str))
     {
-      //Verifico que no haya cambiado mientras calculaba
+      // Verifico que no haya cambiado mientras calculaba
       if (last_block_in_chain->index < block.index)
       {
         mined_blocks += 1;
@@ -155,24 +154,24 @@ void *proof_of_work(void *ptr)
 
 int node()
 {
-  //Tomar valor de mpi_rank y de nodos totales
+  // Tomar valor de mpi_rank y de nodos totales
   MPI_Comm_size(MPI_COMM_WORLD, &total_nodes);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-  //La semilla de las funciones aleatorias depende del mpi_ranking
+  // La semilla de las funciones aleatorias depende del mpi_ranking
   srand(time(NULL) + mpi_rank);
   printf("[MPI] Lanzando proceso %u\n", mpi_rank);
 
   last_block_in_chain = new Block;
 
-  //Inicializo el primer bloque
+  // Inicializo el primer bloque
   last_block_in_chain->index = 0;
   last_block_in_chain->node_owner_number = mpi_rank;
   last_block_in_chain->difficulty = DEFAULT_DIFFICULTY;
   last_block_in_chain->created_at = static_cast<unsigned long int>(time(NULL));
   memset(last_block_in_chain->previous_block_hash, 0, HASH_SIZE);
 
-  //Creo thread para minar
+  // Creo thread para minar
   pthread_t thread;
   pthread_create(&thread, nullptr, proof_of_work, nullptr);
 
